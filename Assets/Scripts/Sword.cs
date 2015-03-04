b0c60e5e671788b965052cd4a0d99a6f36c5e881
HippolyteMaes <hippo.maes@gmail.com>
2015-03-04 19:59:30 +0100

    animation attaque moche
    très moche

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -9,30 +9,46 @@ public class Sword : MonoBehaviour {
 
 	GameObject player;
 	PlatformerCharacter2D dir;
+	float timer;
 
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
 		dir = player.GetComponent<PlatformerCharacter2D> ();
+		renderer.enabled = false;
+		timer = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Q))
+		if (timer != 0) {
+			timer += Time.deltaTime;
+			if(timer > 1.25)
+			{
+				timer = 0;
+				renderer.enabled = false;
+			}
+		}	
+		else if (Input.GetKeyDown (KeyCode.Q))
 						attack ();
+
 	}
 
 	private void attack() {
 
+		renderer.enabled = true;
+		timer = 1;
+
 		RaycastHit2D hit;
 
 		if (dir.facingRight)
-			hit = Physics2D.Raycast (transform.position, Vector2.right);
+			hit = Physics2D.Raycast (transform.position, Vector2.right, MaxDistance);
 		else
-			hit = Physics2D.Raycast (transform.position, -Vector2.right);
+			hit = Physics2D.Raycast (transform.position, -Vector2.right, MaxDistance);
 
 
-		if (hit.collider != null && Mathf.Abs (hit.point.x - transform.position.y) < MaxDistance && hit.transform.gameObject.tag == "Enemy") {
+		if (hit.collider != null && hit.transform.gameObject.tag == "Enemy") 
+		{
 			GameObject enemy = hit.transform.gameObject;
 			EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
 			enemyHealth.TakeDamage(attackDamage);
