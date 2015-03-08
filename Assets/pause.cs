a77006bd9df53562d56551851ccf04312db22df3
HippolyteMaes <hippo.maes@gmail.com>
2015-03-08 17:58:59 +0100

    pause
    pause sans menu

---
+++ Assets/pause.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+
+	private bool paused;
+
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("escape"))
+			paused = !paused;
+		if (paused)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+}
