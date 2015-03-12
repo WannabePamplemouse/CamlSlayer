2d71f6d7a2dccff6e58cc7af60d403b5398be0a0
HippolyteMaes <hippo.maes@gmail.com>
2015-03-12 17:12:22 +0100

    Barre d'energie
    fonctionne avec l'attque de base

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerEnergy : MonoBehaviour {
+
+	public Slider EnergySlider;
+	public int stratingEnergy = 100;
+	public int currentEnergy;
+	float timer;
+
+	// Use this for initialization
+	void Awake () {
+		currentEnergy = stratingEnergy;
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 1 && currentEnergy < 100) {
+			timer = 0;
+			currentEnergy += 10;
+			if(currentEnergy > 100)
+			{
+				currentEnergy = 100;
+			}
+		}
+		EnergySlider.value = currentEnergy;
+
+	}
+
+	public void UseEnergy(int amount)
+	{
+		currentEnergy -= amount;
+		if (currentEnergy < 0) {
+			currentEnergy = 0;
+		}
+		EnergySlider.value = currentEnergy;
+	}
+}
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,17 +4,20 @@ using UnitySampleAssets._2D;
 
 public class Sword : MonoBehaviour {
 
-	public float MaxDistance = 1000000000000;
-	public int attackDamage = 100;
+	public float MaxDistance;
+	public int attackDamage;
+	public int EnergyCost;
 
 	GameObject player;
 	PlatformerCharacter2D dir;
 	float timer;
+	PlayerEnergy energy;
 
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
 		dir = player.GetComponent<PlatformerCharacter2D> ();
+		energy = player.GetComponent<PlayerEnergy>();
 		renderer.enabled = false;
 		timer = 0;
 	}
@@ -36,22 +39,26 @@ public class Sword : MonoBehaviour {
 
 	private void attack() {
 
-		renderer.enabled = true;
-		timer = 1;
+		if (energy.currentEnergy >= EnergyCost) {
+			energy.UseEnergy (EnergyCost);
 
-		RaycastHit2D hit;
+			renderer.enabled = true;
+			timer = 1;
 
-		if (dir.facingRight)
-			hit = Physics2D.Raycast (transform.position, Vector2.right, MaxDistance);
-		else
-			hit = Physics2D.Raycast (transform.position, -Vector2.right, MaxDistance);
+			RaycastHit2D hit;
+
+			if (dir.facingRight)
+				hit = Physics2D.Raycast (transform.position, Vector2.right, MaxDistance);
+			else
+				hit = Physics2D.Raycast (transform.position, -Vector2.right, MaxDistance);
 
 
-		if (hit.collider != null && hit.transform.gameObject.tag == "Enemy") 
-		{
-			GameObject enemy = hit.transform.gameObject;
-			EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
-			enemyHealth.TakeDamage(attackDamage);
+			if (hit.collider != null && hit.transform.gameObject.tag == "Enemy") 
+			{
+				GameObject enemy = hit.transform.gameObject;
+				EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
+				enemyHealth.TakeDamage(attackDamage);
+			}
 		}
 	}
 }
