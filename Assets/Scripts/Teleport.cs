12173bcf4e77a28651cccf1d49eb285266bc6b30
Hazriel <gabriel.bertini@epita.fr>
2015-03-17 21:11:09 +0100

    Portails
    J'ai fait en sorte que l'on puisse revenir dans les salles, je vais
    virer ma vieille caverne de merde car on ne peut pas revenir.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,9 +5,11 @@ public class Teleport : MonoBehaviour {
 	
 	public Transform target;
 	public int enemiesToKill;
+	public bool teleported;
 
 	GameObject player;
 	KillCount KC;
+	GameObject portal;
 
 	void Awake()
 	{
@@ -16,9 +18,16 @@ public class Teleport : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
-		if (other.tag == "Player" && KC.enemyKilled >= enemiesToKill) {
+		if (other.tag == "Player" && KC.enemyKilled >= enemiesToKill && !teleported) {
 
 			other.gameObject.transform.position = target.transform.position;
+			target.GetComponent<Teleport>().teleported = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.tag == "Player") {
+			teleported = false;
 		}
 	}
 }
