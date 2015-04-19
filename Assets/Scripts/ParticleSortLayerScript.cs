68cf7fcf6d84f5d3e4adcab30ada3400331f39ef
Hazriel <gabriel.bertini@epita.fr>
2015-04-19 12:43:14 +0200

    Push yolo

---
+++ Assets/Scripts/ParticleSortLayerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSortLayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Ground";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
