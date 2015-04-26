9e5129db0460ef02f0462e32ff46b7a9225a12f6
Ringokilol <nicolas.lugassy@epita.fr>
2015-04-26 19:41:17 +0200

    Add animation
    ajout des animations de bomb/épée/tromblon mais des beugs encore
    présents à fixer

---
+++ Assets/RobotControllerScript.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotControllerScript : MonoBehaviour {
+
+	private float maxSpeed = 10f;
+	bool facingRight = true;
+	
+	Animator anim;
+	
+	bool grounded = false;
+	private Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	private float jumpForce = 700f;
+	
+	float timer;
+
+	public bool isWorld1finished = false;
+	public bool isWorld2finished = false;
+	public bool isWorld3finished = false;
+	public bool isWorld4finished = false;
+
+	public bool haveSword = true;
+	public bool haveBomb = false;
+	public bool haveTromblon = false;
+
+
+	public void SwitchBomb()
+	{
+		haveBomb = true;
+		haveSword = false;
+		haveTromblon = false;
+		anim.SetBool ("haveBomb", true);
+	}
+
+	public void SwitchTromblon()
+	{
+		haveTromblon = true;
+		haveBomb = false;
+		haveSword = false;
+		anim.SetBool ("haveTromblon", true);
+	}
+
+	public void SwitchSword()
+	{
+		haveSword = true;
+		haveBomb = false;
+		haveTromblon = false;
+		anim.SetBool ("haveSword", true);
+	}
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		groundCheck = transform.Find("GroundCheck");
+	}
+	
+	
+	void FixedUpdate () 
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+		
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+		
+		
+		float move = Input.GetAxis ("Horizontal");
+		
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+	
+	void Update()
+	{
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
+			grounded = false;
+			anim.SetBool("Ground",false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+
+		if(Input.GetKeyDown(KeyCode.B))
+			SwitchBomb();
+
+		if (isWorld1finished && Input.GetKeyDown (KeyCode.T))
+			SwitchTromblon();
+
+		if (Input.GetKeyDown (KeyCode.S))
+			SwitchSword ();
+
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			timer = 0;
+			anim.SetBool ("isAttacking", true);
+		}
+		
+		timer += Time.deltaTime;
+		
+		if(timer > 0.50)
+			anim.SetBool ("isAttacking", false);
+	}
+	
+	void Flip()
+	{
+		facingRight = !facingRight;
+		
+		Vector3 theScale = transform.localScale;	
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
