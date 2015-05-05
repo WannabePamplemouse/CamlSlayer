d8147fdab5d42aa61977e8ff36419e3472835af4
Ringokilol <nicolas.lugassy@epita.fr>
2015-05-05 02:49:33 +0200

    musique boss3 + boutons menu mort/pause

---
+++ Assets/World3SoundControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World3SoundControllerScript : MonoBehaviour {
+
+	public AudioClip newClip;
+	public Transform RobotPosition;
+	private bool toplay = true;
+
+	void Update () 
+	{
+		if (toplay && RobotPosition.position.x > 2450) 
+		{
+			toplay = false;
+			audio.clip = newClip;
+			audio.Play ();
+		}
+	}
+}
