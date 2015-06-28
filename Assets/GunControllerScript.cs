cfd227855c9885f4ce9a243b16a249aa816f73e1
HippolyteMaes <hippo.maes@gmail.com>
2015-06-29 00:46:18 +0200

    truc multi

--- Assets/GunControllerScript.cs
+++ Assets/GunControllerScript.cs
@@ -1,69 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GunControllerScript : MonoBehaviour {
-	
-	public float fireRate = 0;
-	public float Damages = 10;
-	public LayerMask whatToHit;
-	public GameObject toshot;
+public class GunControllerScript : MonoBehaviour
+{
 
-	private float timeToFire = 0;
-	private Transform gunPoint;
+    public float fireRate = 0;
+    public float Damages = 10;
+    public LayerMask whatToHit;
+    public GameObject toshot;
 
-	public static GameObject player;
-	Transform target;
+    private float timeToFire = 0;
+    private Transform gunPoint;
+
+    private GameObject player;
+    Transform target;
 
 
-	void Awake () 
-	{
-		gunPoint = GameObject.FindGameObjectWithTag ("MultiGun").gameObject.transform;
-		Physics2D.IgnoreLayerCollision (8, gameObject.layer);
-		if (gunPoint == null)
-			Debug.Log ("No gun found");
-	}
-	
+    void Awake()
+    {
+        gunPoint = GameObject.FindGameObjectWithTag("MultiGun").gameObject.transform;
+        Physics2D.IgnoreLayerCollision(8, gameObject.layer);
+        if (gunPoint == null)
+            Debug.Log("No gun found");
 
-	void Update () 
-	{
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z);
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+    }
 
-		if (!networkView.isMine)
-			return;
 
-		if (fireRate == 0) 
-		{
-			if (Input.GetKeyDown (KeyCode.U))
-				Shoot ();
-		}
-		else 
-		{
-			if(Input.GetKeyDown (KeyCode.U) && Time.time > timeToFire)
-			{
-				timeToFire = Time.time + 1/fireRate;
-				Shoot();
-			}
-		}
-	}
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y + 3, target.position.z);
 
-	private void Shoot()
-	{
-		Vector2 mousePos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
-		Vector2 firePos = new Vector2 (gunPoint.position.x, gunPoint.position.y);
-		GameObject _shoot = (GameObject)Instantiate (toshot, transform.position, new Quaternion (0, 0, 0, 0));
-		_shoot.rigidbody2D.velocity = ((mousePos - firePos) * 5);
-		//Debug.Log ("Mouse position in x = " + mousePos.x + "Mouse position in y = " + mousePos.y);
-		//RaycastHit2D hit = Physics2D.Raycast (firePos, (mousePos - firePos), 100, whatToHit);
-		//Debug.DrawLine (firePos, (mousePos - firePos) * 100, Color.cyan);
-		/*Debug.Log ("Shooting dont know where");
-		if (hit.collider != null) 
-		{
-			Debug.DrawLine(firePos, hit.point, Color.red);
-			Debug.Log("We hit" + hit.collider.name);
-		}
-		/*Vector2 dir = (target.position - transform.position).normalized;
-		GameObject shoot_bullet = (GameObject)Instantiate(toshot, transform.position, new Quaternion(0, 0, 0, 0));
-		shoot_bullet.rigidbody2D.velocity = dir * 25;*/
+        if (!networkView.isMine)
+            return;
 
-	}
+        if (fireRate == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+                Shoot();
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.U) && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePos = new Vector2(gunPoint.position.x, gunPoint.position.y);
+        GameObject _shoot = (GameObject)Instantiate(toshot, transform.position, new Quaternion(0, 0, 0, 0));
+        _shoot.rigidbody2D.velocity = ((mousePos - firePos) * 5);
+        //Debug.Log ("Mouse position in x = " + mousePos.x + "Mouse position in y = " + mousePos.y);
+        //RaycastHit2D hit = Physics2D.Raycast (firePos, (mousePos - firePos), 100, whatToHit);
+        //Debug.DrawLine (firePos, (mousePos - firePos) * 100, Color.cyan);
+        /*Debug.Log ("Shooting dont know where");
+        if (hit.collider != null) 
+        {
+            Debug.DrawLine(firePos, hit.point, Color.red);
+            Debug.Log("We hit" + hit.collider.name);
+        }
+        /*Vector2 dir = (target.position - transform.position).normalized;
+        GameObject shoot_bullet = (GameObject)Instantiate(toshot, transform.position, new Quaternion(0, 0, 0, 0));
+        shoot_bullet.rigidbody2D.velocity = dir * 25;*/
+
+    }
 }
--- Assets/Resources/NetworkManager2.cs
+++ Assets/Resources/NetworkManager2.cs
@@ -3,118 +3,118 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 
-public class NetworkManager2 : MonoBehaviour {
+public class NetworkManager2 : MonoBehaviour
+{
 
-	string registeredGameName = "WannabeePamplemousse_CamlSlayer_Multiplayer";
-	bool isRefreshing = false;
-	float refreshRequestLength = 3.0f;
-	HostData[] hostData;
+    string registeredGameName = "WannabeePamplemousse_CamlSlayer_Multiplayer";
+    bool isRefreshing = false;
+    float refreshRequestLength = 3.0f;
+    HostData[] hostData;
 
-	public GameObject playerPref;
-	public GameObject robotPref;
+    public GameObject playerPref;
+    public GameObject robotPref;
 
-	public static GameObject Player;
-	public static GameObject Robot;
-	public static bool isMulti;
+    private GameObject Player;
+    private GameObject Robot;
+    public static bool isMulti;
 
-	private float lastSynchronizationTime = 0f;
-	private float syncDelay = 0f;
-	private float syncTime = 0f;
-	private Vector3 syncStartPosition = Vector3.zero;
-	private Vector3 syncEndPosition = Vector3.zero;
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
 
-	void Awake()
-	{
+    void Awake()
+    {
 
-	}
+    }
 
-	private void StartServer()
-	{
-		Network.InitializeServer (5, 25000, false);
-		MasterServer.RegisterHost (registeredGameName, "CamlSlayer Multiplayer", "Here you can test the multiplayer !");
-	}
+    private void StartServer()
+    {
+        Network.InitializeServer(5, 25000, false);
+        MasterServer.RegisterHost(registeredGameName, "CamlSlayer Multiplayer", "Here you can test the multiplayer !");
+    }
 
-	void OnServerInitialized()
-	{
-		Player = (GameObject)Network.Instantiate(playerPref, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-		GunControllerScript.player = Player;
-	}
+    void OnServerInitialized()
+    {
+        Player = (GameObject)Network.Instantiate(playerPref, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+    }
 
-	void OnMasterServerEvent(MasterServerEvent masterServerEvent)
-	{
-		if (masterServerEvent == MasterServerEvent.RegistrationSucceeded)
-			Debug.Log ("Registration successfull !");
-	}
+    void OnMasterServerEvent(MasterServerEvent masterServerEvent)
+    {
+        if (masterServerEvent == MasterServerEvent.RegistrationSucceeded)
+            Debug.Log("Registration successfull !");
+    }
 
-	public IEnumerator RefreshHostList()
-	{
-		MasterServer.RequestHostList (registeredGameName);
-		float timeStarted = Time.time;
-		float timeEnd = Time.time + refreshRequestLength;
+    public IEnumerator RefreshHostList()
+    {
+        MasterServer.RequestHostList(registeredGameName);
+        float timeStarted = Time.time;
+        float timeEnd = Time.time + refreshRequestLength;
 
-		while (Time.time < timeEnd) 
-		{
-			hostData = MasterServer.PollHostList();
-			yield return new WaitForEndOfFrame();
-		}
-	}
+        while (Time.time < timeEnd)
+        {
+            hostData = MasterServer.PollHostList();
+            yield return new WaitForEndOfFrame();
+        }
+    }
 
-	private void JoinServer(HostData hostData)
-	{
-		Network.Connect(hostData);
-	}
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
 
-	void OnConnectedToServer()
-	{
-		Robot = (GameObject)Network.Instantiate(robotPref, new Vector3(0f, 10f, 0f), Quaternion.identity, 0);
-	}
+    void OnConnectedToServer()
+    {
+        Robot = (GameObject)Network.Instantiate(robotPref, new Vector3(0f, 10f, 0f), Quaternion.identity, 0);
+    }
 
-	void OnPlayerDisconnected(NetworkPlayer player)
-	{
-		Network.DestroyPlayerObjects (player);
-	}
+    void OnPlayerDisconnected(NetworkPlayer player)
+    {
+        Network.DestroyPlayerObjects(player);
+    }
 
-	void OnApplicationQuit()
-	{
-		if (Network.isServer) 
-		{
-			Network.Disconnect (200);
-			MasterServer.UnregisterHost ();
-		} 
+    void OnApplicationQuit()
+    {
+        if (Network.isServer)
+        {
+            Network.Disconnect(200);
+            MasterServer.UnregisterHost();
+        }
 
-		if(Network.isClient)
-			Network.Disconnect (200);
+        if (Network.isClient)
+            Network.Disconnect(200);
 
-	}
+    }
 
-	void Update()
-	{
-	}
-	
-	void OnGUI()
-	{
-		if (!Network.isServer && !Network.isClient) 
-		{
-			if (GUI.Button (new Rect (25f, 25f, 150f, 30f), "Start new Server")) 
-			{
-				StartServer ();
-			}
+    void Update()
+    {
+    }
 
-			if (GUI.Button (new Rect (25f, 65f, 150f, 30f), "Refresh Server List")) 
-			{
-				StartCoroutine ("RefreshHostList");
-			}
+    void OnGUI()
+    {
+        if (!Network.isServer && !Network.isClient)
+        {
+            if (GUI.Button(new Rect(25f, 25f, 150f, 30f), "Start new Server"))
+            {
+                StartServer();
+            }
 
-			if (hostData != null) 
-			{
-				for (int i = 0; i < hostData.Length; i++) 
-				{
-					if (GUI.Button (new Rect (Screen.width / 2, 65f + (30f * i), 300f, 30f), hostData [i].gameName)) 
-					{
-						JoinServer(hostData[i]);
-					}
-				}
-			}
-		}
-	}
+            if (GUI.Button(new Rect(25f, 65f, 150f, 30f), "Refresh Server List"))
+            {
+                StartCoroutine("RefreshHostList");
+            }
+
+            if (hostData != null)
+            {
+                for (int i = 0; i < hostData.Length; i++)
+                {
+                    if (GUI.Button(new Rect(Screen.width / 2, 65f + (30f * i), 300f, 30f), hostData[i].gameName))
+                    {
+                        JoinServer(hostData[i]);
+                    }
+                }
+            }
+        }
+    }
 }
