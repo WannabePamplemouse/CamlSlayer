f77027400f58bd0a8da6563dff8cf87aca0a6d4a
Ringokilol <nicolas.lugassy@epita.fr>
2015-06-30 01:57:16 +0200

    add musique monde 4

---
+++ Assets/World4SoundController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World4SoundController : MonoBehaviour {
+
+	public AudioClip newClip;
+	public AudioClip OldClip;
+	public Transform RobotPosition;
+	private bool toplay = true;
+	private bool toplayagain = true;
+	
+	void Awake()
+	{
+		if (UIManagerScript.volumeValue == 0f)
+			audio.volume = UIManagerScript.defaultValue;
+		else
+			audio.volume = UIManagerScript.volumeValue;
+	}
+	void Update () 
+	{
+		if (InGameCommandController.isAvailable)
+			audio.volume = InGameCommandController.volumeValue;
+		
+		if (toplay && RobotPosition.position.x > 1800) 
+		{
+			toplay = false;
+			audio.clip = newClip;
+			audio.Play ();
+		}
+		if (toplayagain && UIManagerScript.isWorld4finished) 
+		{
+			toplayagain = false;
+			audio.clip = OldClip;
+			audio.Play();
+		}
+	}
+}
